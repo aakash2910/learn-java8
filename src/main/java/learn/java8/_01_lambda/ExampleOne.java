f8bde8f36dc33123e55827d88991416adc0001a8
aakash2910 <aakashjiyani2910@gmail.com>
2024-10-05 22:31:23 -0600

    Add lambda example one

---
+++ src/main/java/learn/java8/_01_lambda/ExampleOne.java
@@ -0,0 +1,16 @@
+package learn.java8._01_lambda;
+
+@FunctionalInterface
+interface Printable
+{
+	void print(String message);
+}
+
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		
+		Printable p = message -> System.out.println("Message is : " + message);
+		p.print("Hello Lambda");
+	}
+}
