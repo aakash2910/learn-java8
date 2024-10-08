59f1fc77fac9449636b7e6e7eeab404a8b8b0a0c
aakash2910 <aakashjiyani2910@gmail.com>
2024-10-07 23:09:19 -0600

    Add lamda examples

---
+++ src/main/java/learn/java8/_01_lambda/ExampleFive.java
@@ -0,0 +1,37 @@
+package learn.java8._01_lambda;
+
+
+// Thread example using lambda instead of anonymous inner class
+
+public class ExampleFive {
+
+	public static void main(String[] args) 
+	{
+		
+		/*Runnable r = new Runnable()
+		{
+			public void run()
+			{
+				for(int i=0; i<10; i++)
+				{
+					System.out.println("Child thread");
+				}
+			}
+		};*/
+		
+		Runnable r = () -> {
+			for(int i=0; i<10; i++)
+			{
+				System.out.println("Child thread");
+			}
+		};
+		
+		Thread t = new Thread(r);
+		t.start();
+		
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("Main thread");
+		}
+	}
+}
---
+++ src/main/java/learn/java8/_01_lambda/ExampleFour.java
@@ -0,0 +1,20 @@
+package learn.java8._01_lambda;
+
+import java.util.TreeSet;
+
+// TreeSet descending order sort using lambda
+
+public class ExampleFour {
+
+	public static void main(String[] args) {
+		
+		TreeSet<Integer> numbers = new TreeSet<Integer>((num1,num2) -> (num1 > num2) ? -1 : (num1 < num2) ? 1 : 0);
+		numbers.add(30);
+		numbers.add(20);
+		numbers.add(40);
+		numbers.add(10);
+		numbers.add(50);
+		
+		System.out.println(numbers);
+	}
+}
